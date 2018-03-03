a4d711161d18e991a087e90f80e227804caa4d41
Patryk <patryk@student.edu>
2018-03-03 18:02:28 +0100

    Initial commit

---
+++ src/main/java/com/hsbc/pk/hsbc_challange2/HsbcChallange2Application.java
@@ -0,0 +1,12 @@
+package com.hsbc.pk.hsbc_challange2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcChallange2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsbcChallange2Application.class, args);
+	}
+}
---
+++ src/test/java/com/hsbc/pk/hsbc_challange2/HsbcChallange2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsbc.pk.hsbc_challange2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HsbcChallange2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
