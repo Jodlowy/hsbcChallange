5dfafb3283ca3dcacb204efaa20f86ed317524e9
yodlowy <Patryk.hb@gmail.com>
2018-03-06 22:51:28 +0100

    deleting out and adding service interfaces

---
+++ src/main/java/com/hsbc/pk/hsbc_challange2/service/TwitterService.java
@@ -0,0 +1,4 @@
+package com.hsbc.pk.hsbc_challange2.service;
+
+public interface TwitterService {
+}
---
+++ src/main/java/com/hsbc/pk/hsbc_challange2/service/UserService.java
@@ -0,0 +1,7 @@
+package com.hsbc.pk.hsbc_challange2.service;
+
+public interface UserService{
+}
+
+
+
